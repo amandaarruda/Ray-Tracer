f48d6d64967f08a9446e94cf223ef10c51d25477
Arthur Lima <arl3@cin.ufpe.br>
2024-05-17 19:21:14 -0300

    Add ray cast function, image generator

--- Projeto/main.cpp
+++ Projeto/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
-#include "./Includes/ray.h"   
-#include "./Includes/color.h" 
+#include "../External/glm/glm.hpp"
+#include "../External/glm/gtc/matrix_transform.hpp"
+#include "./Includes/ray.h"
+#include "./Includes/color.h"
+#include "./Includes/hitable.h"
+#include "./Includes/sphere.h"
+#include "./Includes/hitable_list.h"
+#include "./Includes/plane.h"
 #include "./Includes/camera.h"
+#include <cmath>
+#include "float.h"
 
 #define M_PI 3.14159265358979323846  // Define a constante M_PI como o valor de pi
 
@@ -10,21 +18,49 @@ const color red = glm::vec3(255.99, 0.0, 0.0);
 const color green = glm::vec3(0.0, 255.99, 0.0);
 const color blue = glm::vec3(0.0, 0.0, 255.99);
 
+// Função para calcular a cor de um raio, dependendo se ele atinge algum objeto no mundo ou não
+color ray_color(const ray& r, hitable *world)
+{
+    hit_record rec;
+    if(world->hit(r, 0.0f, FLT_MAX, rec)){  // Testa se o raio atinge algum objeto no mundo
+        return rec.cor;  // Retorna a cor do objeto atingido pelo raio
+    }
+
+    color backgroundColor = glm::vec3(0.0,0.0,0.0); // Cor preta para o background
+    return backgroundColor;  // Retorna a cor de fundo se o raio não atingir nenhum objeto
+}
 
 int main() {
-    int nx = 500; // Largura da imagem
-    int ny = 500; // Altura da imagem
+    int nx = 500;  // Largura da imagem
+    int ny = 500;  // Altura da imagem
 
-    std::cout << "P3\n" << nx << " " << ny << "\n255\n"; // Imprime o cabeçalho do arquivo PPM
+    std::cout << "P3\n" << nx << " " << ny << "\n255\n";  // Imprime o cabeçalho do arquivo PPM
 
     glm::vec3 origin(0.0f, 0.0f, 0.0f);  // Origem da câmera
     glm::vec3 lookingat(0.0f, 0.0f, -1.0f);  // Ponto para onde a câmera está olhando
     glm::vec3 vup(0.0f, 1.0f, 0.0f);  // Vetor de "up" da câmera
     float distance = 0.3f;  // Distância entre a câmera e o plano da imagem
 
-  // proximas coisas aqui
+    // Cria uma lista de objetos hitable, incluindo duas esferas e dois planos
+    hitable* list[4];
+    list[0] = new sphere(glm::vec3(0, 0, -1), 0.5, red);
+    list[1] = new sphere(glm::vec3(0, -100.5, -1), 100, green);
+    list[2] = new plane(glm::vec3(0, 0, -5), glm::vec3(0, 0, 1), blue);
+    list[3] = new plane(glm::vec3(0, 0, 0), glm::vec3(0, -1, 0), blue);
+    hitable* world = new hitable_list(list, 4);  // Cria um mundo contendo todos os objetos hitable
+
     camera cam(origin, lookingat, vup, ny, nx, distance);  // Cria uma câmera
 
+    // Loop para gerar a imagem linha por linha
+    for (int j = ny - 1; j >= 0; j--) {
+        for (int i = 0; i < nx; i++) {
+            float u = float(i) / float(nx);  // Coordenada u do pixel normalizada
+            float v = float(j) / float(ny);  // Coordenada v do pixel normalizada
+            ray r = cam.get_ray(u, v);  // Obtém o raio correspondente ao pixel na câmera
+            color pixel_color = ray_color(r, world);  // Calcula a cor do raio
+            write_color(std::cout, pixel_color);  // Escreve a cor no arquivo PPM
+        }
+    }
 
-    return 0; // Retorna 0 para indicar que o programa foi executado com sucesso
+    return 0;  // Retorna 0 para indicar que o programa foi executado com sucesso
 }
