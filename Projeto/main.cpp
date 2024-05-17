7ae80ef6a53228fcd7782122a372cdaf47e8229f
Amanda Arruda <66084295+amandaarruda@users.noreply.github.com>
2024-05-17 15:17:56 -0300

    Create main.cpp

---
+++ Projeto/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "./Includes/ray.h"   
+#include "./Includes/color.h" 
+
+int main() {
+    int nx = 500; // Largura da imagem
+    int ny = 500; // Altura da imagem
+
+    std::cout << "P3\n" << nx << " " << ny << "\n255\n"; // Imprime o cabeçalho do arquivo PPM
+
+  // proximas coisas aqui
+
+        }
+    }
+
+    return 0; // Retorna 0 para indicar que o programa foi executado com sucesso
+}
