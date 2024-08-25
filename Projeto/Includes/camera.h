9779dfcde278c9a5b6b07ed9fb0f0fbd9088b823
Amanda Arruda <66084295+amandaarruda@users.noreply.github.com>
2024-08-24 22:15:44 -0300

    fix: get_ray

--- Projeto/Includes/camera.h
+++ Projeto/Includes/camera.h
@@ -1,27 +1,23 @@
-#ifndef CAMERA_H  // Diretiva de pré-processador para garantir que o arquivo só será incluído uma vez
+#ifndef CAMERA_H
 #define CAMERA_H
 
-#include "ray.h"  // Inclui o arquivo de cabeçalho para a classe ray
-#include "../../External/glm/glm.hpp"  // Inclui o arquivo de cabeçalho para a biblioteca glm
+#include "ray.h"  
+#include "../../External/glm/glm.hpp"  
 
 class camera {
 public:
-    // Construtor da classe camera
+    // Construtor da classe camera que inicializa a câmera com base na posição, direção e configuração da imagem.
     camera(glm::vec3 lookfrom, glm::vec3 lookat, glm::vec3 vup, float vres, float hres, float dist);
 
-    // Método para obter um raio que passa pelo ponto (s, t) no plano da imagem
-    ray get_ray(float s, float t);
-
-    // Método para obter a origem da câmera
-    glm::vec3 get_origin() const { return origem; }
+    // Método que gera um raio com base nas coordenadas (s, t) do plano da imagem.
+    ray get_ray(float s, float t) const;
 
 private:
-    glm::vec3 origem;  // Ponto de origem da câmera
-    glm::vec3 lower_left_corner;  // Canto inferior esquerdo do plano da imagem
-    glm::vec3 horizontal;  // Vetor horizontal que define a largura do plano da imagem
-    glm::vec3 vertical;  // Vetor vertical que define a altura do plano da imagem
-    glm::vec3 u, v, w;  // Vetores ortonormais que definem a base da câmera
+    glm::vec3 origem;  // Ponto de origem da câmera (localização da câmera).
+    glm::vec3 lower_left_corner;  // Canto inferior esquerdo do plano da imagem na cena.
+    glm::vec3 horizontal;  // Vetor que representa a largura do plano da imagem.
+    glm::vec3 vertical;  // Vetor que representa a altura do plano da imagem.
+    glm::vec3 u, v, w;  // Vetores de base para o sistema de coordenadas da câmera: 'u' é o vetor para a direita, 'v' é o vetor para cima, e 'w' é o vetor para frente (direção da visão).
 };
 
-#endif  // Fecha a diretiva de pré-processador
-
+#endif
